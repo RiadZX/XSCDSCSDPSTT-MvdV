272dd3545e6920b2966b8a94c1662699f6ce1cef
Stijn <risseeuw.stijn@gmail.com>
2024-05-25 16:22:51 +0200

    java!

---
+++ src/main/kotlin/com/github/riadzx/xscdscsdpsttmvdv/toolWindow/ComplexityWindowFactory.java
@@ -0,0 +1,63 @@
+package com.github.riadzx.xscdscsdpsttmvdv.toolWindow;
+
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.github.riadzx.xscdscsdpsttmvdv.MyBundle;
+import com.github.riadzx.xscdscsdpsttmvdv.services.MyProjectService;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComplexityWindowFactory implements ToolWindowFactory {
+
+    private static final Logger LOGGER = Logger.getInstance(ComplexityWindowFactory.class);
+
+    public ComplexityWindowFactory() {
+        LOGGER.warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.");
+    }
+
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+        ComplexityWindow myToolWindow = new ComplexityWindow(toolWindow);
+        ContentFactory contentFactory = ContentFactory.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), null, false);
+        toolWindow.getContentManager().addContent(content);
+    }
+
+    @Override
+    public boolean shouldBeAvailable(Project project) {
+        return true;
+    }
+
+    private static class ComplexityWindow {
+
+        private final MyProjectService service;
+
+        ComplexityWindow(ToolWindow toolWindow) {
+            this.service = toolWindow.getProject().getService(MyProjectService.class);
+        }
+
+        public JPanel getContent() {
+            JBPanel<JBPanel<?>> panel = new JBPanel<>();
+            JButton button = new JButton(MyBundle.message("scanActiveFile"));
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // get the currently opened file
+                    service.getCurrentFile();
+                }
+            });
+            panel.add(button);
+            return panel;
+        }
+    }
+}
