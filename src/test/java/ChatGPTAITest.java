728149b88bf695e51150f90f8ee9064232f48363
Riad Zaid <r.zaid@student.tudelft.nl>
2024-05-25 17:12:37 +0200

    ChatGPT worky worky

--- src/test/java/ChatGPTAITest.java
+++ src/test/java/ChatGPTAITest.java
@@ -5,8 +5,18 @@ public class ChatGPTAITest {
     @Test
     public void testGetResponse() {
         ChatGPTAI chatGPTAI = new ChatGPTAI();
-        String response = chatGPTAI.getResponse("Hello");
-        System.out.println(ChatGPTAI.extractMessageFromJSONResponse(response));
+        String response = chatGPTAI.getResponse("""
+                def quicksort(array, low=0, high=None):
+                    if high is None:
+                        high = len(array) - 1
+                                
+                    if low < high:
+                        pivot_index = partition(array, low, high)
+                        quicksort(array, low, pivot_index-1)
+                        quicksort(array, pivot_index+1, high)
+                """);
+        System.out.println(response);
+         System.out.println(ChatGPTAI.extractMessageFromJSONResponse(response));
         assert true;
     }
 }
