f78522f2628aafd93ffdf48909257dc29f5b07dd
GitHub Action <action@github.com>
2024-05-25 12:21:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/riadzx/xscdscsdpsttmvdv/MyBundle.kt
+++ src/main/kotlin/com/github/riadzx/xscdscsdpsttmvdv/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.riadzx.xscdscsdpsttmvdv
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/riadzx/xscdscsdpsttmvdv/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/riadzx/xscdscsdpsttmvdv/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.riadzx.xscdscsdpsttmvdv.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.riadzx.xscdscsdpsttmvdv.MyBundle
+import com.github.riadzx.xscdscsdpsttmvdv.services.MyProjectService
 import javax.swing.JButton
 
 
