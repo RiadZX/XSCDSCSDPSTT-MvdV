bf6d8502cd58814d7db8b7a7b5e405ef2734d1b3
Riad Zaid <r.zaid@student.tudelft.nl>
2024-05-26 11:43:27 +0200

    balalblablab

--- src/main/java/utils/MethodCodeBlockChangeListener.java
+++ src/main/java/utils/MethodCodeBlockChangeListener.java
@@ -4,26 +4,19 @@ import com.intellij.psi.PsiTreeChangeAdapter;
 import com.intellij.psi.PsiTreeChangeEvent;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
+import services.ScanFileService;
 
 public class MethodCodeBlockChangeListener extends PsiTreeChangeAdapter {
 
     @Override
-    public void childAdded(@NotNull PsiTreeChangeEvent event) {
-        PsiElement element = event.getChild();
-        if ("METHOD".equals(element.getNode().getElementType().toString())) {
-            System.out.println("Added method" + element);
-            MethodInfo methodInfo = new MethodInfo(element);
-            Controller.dependencyTree.addMethod(methodInfo);
+    public void childrenChanged(@NotNull PsiTreeChangeEvent event) {
+        PsiElement element = event.getParent();
+        if (element != null) {
+            if(element.getNode() == null) return;
+            if ("java.FILE".equals(element.getNode().getElementType().toString())) {
+                new ScanFileService().scanFile(element.getContainingFile());
+            }
         }
     }
 
-    @Override
-    public void childRemoved(@NotNull PsiTreeChangeEvent event) {
-        PsiElement element = event.getChild();
-        if ("METHOD".equals(element.getNode().getElementType().toString())) {
-            System.out.println("Removed method" + element);
-            MethodInfo methodInfo = Controller.dependencyTree.findMethodInfo(element);
-            Controller.dependencyTree.removeMethod(methodInfo);
-        }
-    }
 }
